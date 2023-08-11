d1fad10c6035ba7a8d6f20d597e2880ff726839a
tmherring <140438427+tmherring@users.noreply.github.com>
2023-08-11 11:47:16 -0500

    Update buffer_usage.sql
    code cleanup, added missing predicate

--- memory/buffer_usage.sql
+++ memory/buffer_usage.sql
@@ -3,10 +3,10 @@ SET NOCOUNT ON;
 -- Need to be cautious with executing this script on servers that exceed 256GB as this has to
 -- aggregate the pages, the more pages available the longer this will take
 SELECT CASE
-         WHEN total.database_id = 32767 THEN 'MSResource'
-         ELSE DB_NAME(total.database_id)
-       END [database_name], clean.memory_mb [clean_memory_mb], dirty.memory_mb [dirty_memory_mb],
-       total.memory_mb [total_memory_mb]
+         WHEN total.[database_id] = 32767 THEN 'MSResource'
+         ELSE DB_NAME(total.[database_id])
+       END [database_name], clean.[memory_mb] [clean_memory_mb], dirty.[memory_mb] [dirty_memory_mb],
+       total.[memory_mb] [total_memory_mb]
   FROM (SELECT [database_id], CAST(COUNT(*) * 0.0078125 AS real) [memory_mb]
           FROM sys.dm_os_buffer_descriptors
          GROUP BY [database_id]) [total]
@@ -14,9 +14,9 @@ SELECT CASE
                FROM sys.dm_os_buffer_descriptors
               WHERE [is_modified] = 0
               GROUP BY [database_id]) [clean]
+    ON clean.[database_id] = total.[database_id]
   LEFT JOIN (SELECT [database_id], CAST(COUNT(*) * 0.0078125 AS real) [memory_mb]
                FROM sys.dm_os_buffer_descriptors
               WHERE [is_modified] = 1
               GROUP BY [database_id]) [dirty]
-    ON dirty.database_id = total.database_id
-GO
+    ON dirty.[database_id] = total.[database_id];
