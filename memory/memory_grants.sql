ef5024f41dcf34f77dd985d0297f36b7ca6c9de7
tmherring <140438427+tmherring@users.noreply.github.com>
2023-08-11 11:42:47 -0500

    Update memory_grants.sql
    changed table alias to mitigate keyword conflict

--- memory/memory_grants.sql
+++ memory/memory_grants.sql
@@ -1,7 +1,7 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SET NOCOUNT ON;
-SELECT deqmg.[session_id], DB_NAME(der.[database_id]) [database_name], COALESCE(CASE des.[host_name] WHEN '' THEN NULL ELSE des.[host_name] END, dec.[client_net_address]),
-       COALESCE(CASE des.[login_name] WHEN '' THEN NULL ELSE des.[login_name] END, des.[original_login_name]) [login_name], deqmg.[granted_memory_kb],
+SELECT deqmg.[session_id], DB_NAME(der.[database_id]) [database_name], COALESCE(CASE es.[host_name] WHEN '' THEN NULL ELSE es.[host_name] END, ec.[client_net_address]),
+       COALESCE(CASE es.[login_name] WHEN '' THEN NULL ELSE es.[login_name] END, es.[original_login_name]) [login_name], deqmg.[granted_memory_kb],
        deqmg.[requested_memory_kb], deqmg.[ideal_memory_kb], deqmg.[used_memory_kb], deqmg.[max_used_memory_kb], deqmg.[dop], deqmg.[request_time],
        CASE
          WHEN deqmg.[grant_time] IS NULL THEN RIGHT('000' + CONVERT(VARCHAR, ((DATEDIFF(MILLISECOND, deqmg.[request_time], GETDATE())) / 1000 / 3600 / 24)), 3) + ' ' +
@@ -14,12 +14,12 @@ SELECT deqmg.[session_id], DB_NAME(der.[database_id]) [database_name], COALESCE(
   FROM sys.dm_exec_query_memory_grants deqmg
   JOIN sys.dm_exec_requests der 
     ON er.[session_id] = mg.[session_id]
-  JOIN sys.dm_exec_sessions des
-    ON des.[session_id] = der.[session_id]
-  JOIN sys.dm_exec_connections dec
-    ON dec.[most_recent_session_id] = des.[session_id]
- CROSS APPLY sys.dm_exec_input_buffer(des.[session_id], der.[request_id]) deib
+  JOIN sys.dm_exec_sessions es
+    ON es.[session_id] = der.[session_id]
+  JOIN sys.dm_exec_connections ec
+    ON ec.[most_recent_session_id] = es.[session_id]
+ CROSS APPLY sys.dm_exec_input_buffer(es.[session_id], der.[request_id]) deib
  CROSS APPLY sys.dm_exec_query_plan(deqmg.[plan_handle]) deqp
- WHERE deqp.[session_id] <> @@SPID
+ WHERE deqmg.[session_id] <> @@SPID
  ORDER BY 1
 OPTION (MAXDOP 1);
