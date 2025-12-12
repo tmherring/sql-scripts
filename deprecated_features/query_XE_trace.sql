0f4920f5abc0feefa42e4bde7b93a72eaa53f690
T. Michael Herring <140438427+tmherring@users.noreply.github.com>
2025-12-12 09:41:01 -0600

    Enhance query_XE_trace.sql with new data extraction
    Added SQL queries to extract and analyze event data from the XE trace file.

--- deprecated_features/query_XE_trace.sql
+++ deprecated_features/query_XE_trace.sql
@@ -15,3 +15,22 @@ SELECT @file_pattern = REVERSE(SUBSTRING(REVERSE(n.value('(File/@name)[1]', 'var
            AND xst.[target_name] = 'event_file'
          WHERE xs.[name] = @trace_name) [trace_file]
  CROSS APPLY [trace_file].[target_data].nodes('EventFileTarget') AS q(n);
+
+-- Perform a raw grab of the XML records from the trace, snagging just the first 100 records
+SELECT TOP 100 CAST(event_data AS xml) [event_data]
+  FROM sys.fn_xe_file_target_read_file(@file_pattern, null, null, null);
+
+-- Perform a more targeted crack of the trace data
+SELECT TOP 100 xevents.[event_data],                                                                                                                      -- Raw XML of a current row
+       xevents.[event_data].value('(event/@name)[1]', 'varchar(128)') [event_type],                                                                       -- Type of event for the current row
+       DATEADD(MINUTE, DATEDIFF(MINUTE, GETUTCDATE(), CURRENT_TIMESTAMP), xevents.[event_data].value('(event/@timestamp)[1]', 'datetime2')) [event_time], -- datetime of the current event, converted to server local
+       DB_NAME(xevents.[event_data].value('(action[@name="database_id"]/value)[1]', 'int')) [database_name],                                              -- database name where the event was targeted
+       xevents.[event_data].value('(action[@name="client_hostname"]/value)[1]', 'nvarchar(256)') [client_hostname],                                       -- client hostname that submitted the request
+       xevents.[event_data].value('(action[@name="client_app_name"]/value)[1]', 'nvarchar(256)') [client_app_name],                                       -- app name that submited the request
+       xevents.[event_data].value('(action[@name="session_id"]/value)[1]', 'int') [session_id],                                                           -- session ID of the request
+       xevents.[event_data].value('(action[@name="username"]/value)[1]', 'nvarchar(128)') [username],                                                     -- user name associated with the request
+       xevents.[event_data].value('(action[@name="sql_text"]/value)[1]', 'nvarchar(max)') [sql_text]                                                      -- full t-sql for the request
+  FROM sys.fn_xe_file_target_read_file(@file_pattern, null, null, null) xe
+ CROSS APPLY (SELECT CAST(xe.[event_data] AS xml) [event_data]) [xevents]
+ ORDER BY [event_time] DESC;
+GO
