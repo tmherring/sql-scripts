d80e902e5fbc6aff7b2bc7a481ca132a9f27e1b4
T. Michael Herring <140438427+tmherring@users.noreply.github.com>
2025-12-12 09:06:11 -0600

    Create extended events trace for deprecated features
    This SQL script creates an extended events trace to monitor T-SQL submissions that utilize deprecated features.

---
+++ deprecated_features/create_extended_events_trace.sql
@@ -0,0 +1,31 @@
+-- Create an extended events trace to identify what T-SQL is being submitted which uses deprecated features
+CREATE EVENT SESSION [find_deprecated_features] ON SERVER
+   ADD EVENT sqlserver.deprecation_announcement (
+       ACTION (
+           sqlserver.client_app_name,
+           sqlserver.client_hostname,
+           sqlserver.database_id,
+           sqlserver.session_id,
+           sqlserver.sql_text,
+           sqlserver.username
+       )
+   ),
+   ADD EVENT sqlserver.deprecation_final_support (
+           sqlserver.client_app_name,
+           sqlserver.client_hostname,
+           sqlserver.database_id,
+           sqlserver.session_id,
+           sqlserver.sql_text,
+           sqlserver.username
+       )
+   )
+   ADD TARGET package0.event_file (
+       SET filename = N'find_deprecated_features',
+           max_file_size = (50)
+   )
+  WITH (
+       MAX_DISPATCH_LATENCY = 5 SECONDS,
+       TRACK_CAUSALITY = ON,
+       STARTUP_STATE = ON
+  );
+GO
