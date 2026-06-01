2e86ab37e80ffe991e93c8553d7ec511126d119e
T. Michael Herring <140438427+tmherring@users.noreply.github.com>
2026-06-01 08:23:26 -0500

    Refactor SQL for text processing
    allow for better readability of the T-SQL text returned

--- sessions/sessions.sql
+++ sessions/sessions.sql
@@ -34,14 +34,33 @@ SELECT sub.[session_id], sub.[state], sub.[percent_complete], sub.[login], sub.[
                                                                                                       ELSE sub.[statement_end_offset]
                                                                                                     END - sub.[statement_start_offset]) / 2 + 1), ''), [text])
                       FROM sys.dm_exec_sql_text([sql_handle])) LIKE 'FETCH API_CURSOR%') THEN
-                 (SELECT t.[text]
+                 (SELECT REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                         REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                         REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                         N'--' + NCHAR(13) + NCHAR(10) + t.[text] +
+                         NCHAR(13) + NCHAR(10) + N'--' COLLATE Latin1_General_Bin1,
+                         NCHAR(31),N'?'),NCHAR(30),N'?'),NCHAR(29),N'?'),NCHAR(28),N'?'),NCHAR(27),N'?'),NCHAR(26),N'?'),NCHAR(25),N'?'),NCHAR(24),N'?'),NCHAR(23),N'?'),
+                         NCHAR(22),N'?'),NCHAR(21),N'?'),NCHAR(20),N'?'),NCHAR(19),N'?'),NCHAR(18),N'?'),NCHAR(17),N'?'),NCHAR(16),N'?'),NCHAR(15),N'?'),NCHAR(14),N'?'),
+                         NCHAR(),N'?'),NCHAR(),N'?'),NCHAR(8),N'?'),NCHAR(7),N'?'),NCHAR(6),N'?'),NCHAR(5),N'?'),NCHAR(4),N'?'),NCHAR(3),N'?'),NCHAR(2),N'?'),NCHAR(1),N'?'),
+                         NCHAR(0),N'?'), N'<?', N'??'), N'?>', N'??') AS [processing-instruction(query)]
                     FROM sys.dm_exec_cursors(sub.[session_id]) cur
-                   CROSS APPLY sys.dm_exec_sql_text(cur.[sql_handle]) t)
-             ELSE (SELECT ISNULL(NULLIF(SUBSTRING([text], (sub.[statement_start_offset] / 2) + 1, (CASE
-                                                                                                    WHEN sub.[statement_end_offset] = -1 THEN DATALENGTH([text])
-                                                                                                    ELSE sub.[statement_end_offset]
-                                                                                                  END - sub.[statement_start_offset]) / 2 + 1), ''), [text])
-                     FROM sys.dm_exec_sql_text(sub.[sql_handle]))
+                   CROSS APPLY sys.dm_exec_sql_text(cur.[sql_handle]) t
+                     FOR XML PATH(''), TYPE)
+             ELSE (SELECT REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                          REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                          REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                          N'--' + NCHAR(13) + NCHAR(10) +
+                          ISNULL(NULLIF(SUBSTRING([text], (sub.[statement_start_offset] / 2) + 1, (CASE
+                                                                                                     WHEN sub.[statement_end_offset] = -1 THEN DATALENGTH([text])
+                                                                                                     ELSE sub.[statement_end_offset]
+                                                                                                   END - sub.[statement_start_offset]) / 2 + 1), ''), [text]) +
+                          NCHAR(13) + NCHAR(10) + N'--' COLLATE Latin1_General_Bin1,
+                          NCHAR(31),N'?'),NCHAR(30),N'?'),NCHAR(29),N'?'),NCHAR(28),N'?'),NCHAR(27),N'?'),NCHAR(26),N'?'),NCHAR(25),N'?'),NCHAR(24),N'?'),NCHAR(23),N'?'),
+                          NCHAR(22),N'?'),NCHAR(21),N'?'),NCHAR(20),N'?'),NCHAR(19),N'?'),NCHAR(18),N'?'),NCHAR(17),N'?'),NCHAR(16),N'?'),NCHAR(15),N'?'),NCHAR(14),N'?'),
+                          NCHAR(),N'?'),NCHAR(),N'?'),NCHAR(8),N'?'),NCHAR(7),N'?'),NCHAR(6),N'?'),NCHAR(5),N'?'),NCHAR(4),N'?'),NCHAR(3),N'?'),NCHAR(2),N'?'),NCHAR(1),N'?'),
+                          NCHAR(0),N'?'), N'<?', N'??'), N'?>', N'??') AS [processing-instruction(query)]
+                     FROM sys.dm_exec_sql_text(sub.[sql_handle])
+                      FOR XML PATH(''), TYPE)
            END
          ELSE NULL
        END [executing_statement]
